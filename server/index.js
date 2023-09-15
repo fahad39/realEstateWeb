f9ebb965f3080c5a3fc937d5cc17b71c0ef30a84
fahad39 <fahadhussain0127@gmail.com>
2023-09-15 17:56:11 +0500

    added scripts, and index js

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express  from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+dotenv.config()
+
+const app=express()
+const PORT=process.env.PORT || 8001
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})

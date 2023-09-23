b579867b19dc193de43fb0a14856493c8892a786
fahad39 <fahadhussain0127@gmail.com>
2023-09-23 10:13:31 +0500

    added Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+     domain={process.env.REACT_APP_AUTH_DOMAIN}
+     clientId={process.env.REACT_APP_CLIENT_ID}
+     authorizationParams={{
+      redirect_uri:"http://localhost:3000/",
+      audience:"http://localhost:8000",
+      scope:"openid profile email"
+     }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 

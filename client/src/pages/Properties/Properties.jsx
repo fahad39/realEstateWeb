c73441e5a512f14b5048be71709b6dd7c30739a6
POWER\fahad.hussain <fahad.hussain@powersoft19.com>
2023-10-03 17:13:21 +0500

    added search functionality

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Properties.css";
 import SearchBar from "../../components/searchBar/SearchBar";
 import useProperties from "../../hooks/useProperties";
 import { PuffLoader } from "react-spinners";
 import PropertyCard from "../../components/propertyCard/PropertyCard";
+import { property } from "lodash";
 
 const Properties = () => {
   const { data, isError, isLoading } = useProperties();
+  const [filter, setFilter] = useState("");
   if (isError) {
     return (
       <div className="wrapper">
@@ -30,11 +32,23 @@ const Properties = () => {
   return (
     <div className="wrapper">
       <div className="flexColStart paddings innerWidth properties-container">
-        <SearchBar />
+        <SearchBar filter={filter} setFilter={setFilter} />
         <div className="paddings flexCenter properties">
-          {data.map((card, i) => (
-            <PropertyCard card={card} key={i} />
-          ))}
+          {
+            // data.map((card, i) => (
+            //   <PropertyCard card={card} key={i} />
+            // ))
+            data
+              .filter(
+                (property) =>
+                  property.title.toLowerCase().includes(filter.toLowerCase()) ||
+                  property.city.toLowerCase().includes(filter.toLowerCase()) ||
+                  property.country.toLowerCase().includes(filter.toLowerCase())
+              )
+              .map((card, i) => (
+                <PropertyCard card={card} key={i} />
+              ))
+          }
         </div>
       </div>
     </div>

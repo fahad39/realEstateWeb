83eb7a380b171caba6b80c908c933020e3641e28
POWER\fahad.hussain <fahad.hussain@powersoft19.com>
2023-10-04 14:57:00 +0500

    remove bug from update Fav

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -17,7 +17,7 @@ export const sliderSettings={
     }
 }
 
-export const updateFavourites=(id,favourites)=>{
+export const updateFavourites=(id,favourites=[])=>{
     if(favourites?.includes(id)){
         return favourites.filter((resId)=>resId!==id)
     }else{
